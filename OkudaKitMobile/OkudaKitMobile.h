caa87f64f37ec2e016c92e6bafade19881194288
Jeremy Agostino <jeremyagost@gmail.com>
2012-06-01 02:40:05 -0400

    Added files used in iOS target

---
+++ OkudaKitMobile/OkudaKitMobile.h
@@ -0,0 +1,9 @@
+//
+//  OkudaKitMobile.h
+//  OkudaKitMobile
+//
+//  Created by Jeremy Agostino on 6/1/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <OkudaKit/OKSyntaxHighlighter.h>
---
+++ OkudaKitMobileTests/OkudaKitMobileTests.m
@@ -0,0 +1,32 @@
+//
+//  OkudaKitMobileTests.m
+//  OkudaKitMobileTests
+//
+//  Created by Jeremy Agostino on 6/1/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import "OkudaKitMobileTests.h"
+
+@implementation OkudaKitMobileTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in OkudaKitMobileTests");
+}
+
+@end
